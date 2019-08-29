533d4fcf9fc5a8315ae3556c72cabb7d8e4832a2
Sebas <seplarui@gmail.com>
2019-08-29 19:14:57 +0200

    Headers in dataApiService

--- src/app/services/data-api.service.ts
+++ src/app/services/data-api.service.ts
@@ -10,8 +10,25 @@ export class DataApiService {
 
   constructor(private http: HttpClient) { }
 
+/*   headers: HttpHeaders = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept':'application/json',
+      'Access-Control-Allow-Headers':'Content-Type',
+      'Access-Control-Allow-Origin':'*'
+    });*/
+
+readonly httpOptions = {
+  headers: new HttpHeaders({
+    'content-type':'application/json',
+    'Access-Control-Allow-Origin': '*'
+  })
+};
+
+
   getHelloWorld() {
     const url_api = 'http://localhost:3000/';
-    return this.http.get(url_api);
+    
+    console.log("Cabeceras: ");
+    return this.http.get(url_api, this.httpOptions);
   }
 }
