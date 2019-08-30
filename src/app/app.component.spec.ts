05458a0788dbfe23058320bb1be7dd3e432d0ea6
Sebas <seplarui@gmail.com>
2019-08-30 23:51:18 +0200

    Changes in app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,12 +3,16 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './components/hello-world/hello-world.component';
 import { DataApiService } from './services/data-api.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule,
+        HttpClientTestingModule
       ],
       declarations: [
         AppComponent,
@@ -20,22 +24,9 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  /*it('should create the app', () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(HelloWorldComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });*/
-
-  /*it(`should have as title 'gestionFront'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('gestionFront');
   });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('p').textContent).toContain('Hola Mundo');
-  });*/
 });
