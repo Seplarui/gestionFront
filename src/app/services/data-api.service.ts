2307e4e1be5d7021be9353f5c80c2952145e447a
Sebas <seplarui@gmail.com>
2019-10-19 12:14:27 +0200

    changed variable

--- src/app/services/data-api.service.ts
+++ src/app/services/data-api.service.ts
@@ -9,9 +9,9 @@ export class DataApiService {
 
   constructor(private http: HttpClient) { }
   readonly headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
-  readonly url_api = 'http://localhost:3000/hola';
+  readonly urlApi = 'http://localhost:3000/hola';
 
   getHelloWorld() {
-    return this.http.get(this.url_api, {headers:this.headers, responseType: 'text'});
+    return this.http.get(this.urlApi, { headers: this.headers, responseType: 'text' });
   }
 }
